0df4e7331a9c267fd4ee59a2ecfcedf43c5f00ce
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-15 18:12:18 +0530

    navigation added and basic css done

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import './Home.scss'
+import React from "react";
+import "./home.scss";
+import Navbar from "../components/Navbar";
 const Home = () => {
-    return (
-        <div className='home'>
-            
-        </div>
-    )
-}
+  return <div className="home">
+      <Navbar></Navbar>
+  </div>;
+};
 
-export default Home
+export default Home;
