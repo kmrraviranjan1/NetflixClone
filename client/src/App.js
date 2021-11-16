55c2fd9e555cdddbc71e0cb967532621d67ccc10
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-16 16:22:24 +0530

    regiter page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.scss';
+import Register from './pages/register/Register';
 // import Home from './pages/home/Home'
-import Watch from './pages/watch/Watch';
+// import Watch from './pages/watch/Watch';
 function App() {
   return (
     // <Home/>
-    <Watch/>
+    // <Watch/>
+    <Register/>
   );
 }
 
