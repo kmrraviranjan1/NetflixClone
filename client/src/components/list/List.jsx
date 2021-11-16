d3439533f4504b1656815a60ab1b325102e014e0
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-16 15:25:58 +0530

    slider added for list item

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./list.scss";
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 import ListItem from "../listitem/ListItem";
 const List = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const listRef = useRef();
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+    console.log("distance:", distance);
+  };
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlinedIcon className='sliderArrow left' />
-        <div className="container">
-          <ListItem />
-          <ListItem />
+        <ArrowBackIosOutlinedIcon
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -23,7 +41,10 @@ const List = () => {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlinedIcon  className='sliderArrow right'/>
+        <ArrowForwardIosOutlinedIcon
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
