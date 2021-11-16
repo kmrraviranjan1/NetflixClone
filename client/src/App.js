18e36563d4c6c970e1520fee0aa9a0ec41b9a05b
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-16 17:44:13 +0530

    login form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.scss';
-import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+// import Register from './pages/register/Register';
 // import Home from './pages/home/Home'
 // import Watch from './pages/watch/Watch';
 function App() {
   return (
     // <Home/>
     // <Watch/>
-    <Register/>
+    // <Register/>
+    <Login/>
   );
 }
 
