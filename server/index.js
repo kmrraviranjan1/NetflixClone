12a4cb70afef621dcbaed08ed5fd3e920dca58fe
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-17 11:08:56 +0530

    user router added with json webtoken verification

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express =require('express')
 const app = express();
 const dotenv =require('dotenv');
-
+const authRoute = require('./src/router/auth.route')
+const userRoute = require('./src/router/user.route')
 const connect = require('./src/config/db')
 dotenv.config()
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.listen(2345,async ()=>{
     await connect()
     console.log('Server is listening on port 2345')
