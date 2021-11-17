4a834622b8ddf985190d7e12b3d4ec828a9bb584
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-17 18:03:56 +0530

    list route added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const dotenv =require('dotenv');
 const authRoute = require('./src/router/auth.route')
 const userRoute = require('./src/router/user.route')
 const movieRoute = require('./src/router/movie.route')
+const listRoute = require('./src/router/list.route')
 const connect = require('./src/config/db')
 dotenv.config()
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/list", listRoute);
 app.listen(2345,async ()=>{
     await connect()
     console.log('Server is listening on port 2345')
