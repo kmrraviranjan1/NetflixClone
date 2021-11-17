431ec5b7e0c5015b483bd1c82b5e12cc20b6260e
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-17 09:10:14 +0530

    model created for user movie and list

---
+++ server/src/models/list.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const ListSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    type: { type: String },
+    genre: { type: String },
+    content: { type: Array },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+module.exports = mongoose.model("list", ListSchema);
